6e3cf3398af47f78c7287d4d09c005efeaa8a9fa
Anika Raghuvanshi <anika.raghuvanshi@coinbase.com>
2022-09-20 13:28:19 -0400

    Add mock erc20

---
+++ contracts/MockERC20.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.8.15;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol
+    ) payable ERC20(name, symbol) {}
+}
--- contracts/Template.sol
+++ contracts/Template.sol
@@ -4,14 +4,9 @@ pragma solidity 0.8.15;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Template {
-    IERC20 public immutable myToken;
+    IERC20 public immutable token;
 
-	constructor(IERC20 token) {
-        myToken = token;
-    }
-
-    function addLiquidity(uint256 liquidityToAdd) external {
-        addLiquiditySuccess = token.transferFrom(msg.sender, address(this), liquidityToAdd);
-        require(addLiquiditySuccess == true, "could not add liquidity");
+	constructor(IERC20 _token) {
+        token = _token;
     }
 }
